e7ff0397ea81cfc1e5cf95577dd0a44dedf96312
gcitdeveloper <44411324+gcitdeveloper@users.noreply.github.com>
2020-07-23 11:47:44 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,19 +1,19 @@
 <?php
 // SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
+//require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+//MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
 // Crea un objeto de preferencia
-$preference = new MercadoPago\Preference();
+//$preference = new MercadoPago\Preference();
 
 // Crea un ítem en la preferencia
-$item = new MercadoPago\Item();
-$item->title = 'Mi producto';
-$item->quantity = 1;
-$item->unit_price = 75.56;
-$preference->items = array($item);
-$preference->save();
+//$item = new MercadoPago\Item();
+//$item->title = 'Mi producto';
+//$item->quantity = 1;
+//$item->unit_price = 75.56;
+//$preference->items = array($item);
+//$preference->save();
 ?>
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -148,12 +148,12 @@ $preference->save();
                                         </h3>
                                     </div>
                                     <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
-                                    <form action="/procesar-pago" method="POST">
+                                    <!--<form action="/procesar-pago" method="POST">
   <script
    src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
    data-preference-id="<?php echo $preference->id; ?>">
   </script>
-</form>
+</form>-->
                                 </div>
                             </div>
                         </div>
